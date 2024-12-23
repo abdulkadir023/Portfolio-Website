ff65e2aa951a283edc8138f6de75fed3f67eba49
Abdul <alole4903@gmail.com>
2024-12-23 04:41:01 +0300

    Storing configuration in the environment separate from code

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
 
 const app = express();
 const port = 3000;
@@ -8,13 +9,14 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+dotenv.config()
 
 const db = new pg.Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'portfolio',
-    password: 'Admin',
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DB,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 });
 
 db.connect();
@@ -42,13 +44,8 @@ app.post('/contactMe', async(req, res) =>{
         console.log(error);
         
     }
-
-
     
-    
-    console.log({name}, {email}, {phone}, {subject}, {message});
-
-    res.json({name}, {email}, {phone}, {subject}, {message});
+    res.redirect('/');
 
 })
 app.listen(port, () =>{
